28fab4bfb6012c1b617dd0ebc64d9240616dd693
jhejhal <jhejhal@sauercompressors.cz>
2025-07-31 11:06:44 +0200

    Update styles.h

--- SW/ESPlan_RS485_ModbusTCP_Gateway/styles.h
+++ SW/ESPlan_RS485_ModbusTCP_Gateway/styles.h
@@ -2,16 +2,20 @@
 #define STYLES_H
 const char styles_css[] PROGMEM = R"rawliteral(
 body{font-family:Helvetica;background:#2e2e2e;color:#fff;text-align:center;margin:0;padding:20px;}
-#container{display:flex;justify-content:center;align-items:flex-start;gap:20px;}
-.settings{display:flex;flex-direction:column;text-align:left;border-right:1px solid #555;padding-right:20px;margin-right:20px;}
+#container{display:flex;justify-content:center;align-items:flex-start;gap:20px;margin-bottom:15px;}
+.settings{display:flex;flex-direction:column;text-align:left;border-right:1px solid #555;padding-right:20px;margin-right:20px;min-width:230px;}
 .field{display:flex;justify-content:space-between;align-items:center;margin-bottom:10px;}
-table{margin:auto;border-collapse:collapse;}
+table{margin:auto;border-collapse:collapse;min-width:700px;}
 td,th{padding:6px;border:1px solid #555;}
-input,button{background:#505050;color:#fff;border:1px solid #888;border-radius:4px;padding:4px 6px;}
+input,button{background:#505050;color:#fff;border:1px solid #888;border-radius:4px;padding:4px 6px;transition:linear 0.1s;}
 input{width:110px;}
-button{padding:4px 10px;cursor:pointer;}
+button,input[type=submit]{padding:4px 10px;cursor:pointer;}
+button:not(.icon):hover,input:hover[type=submit]{color:black;background-color:lightgray;transition:linear 0.1s;}
+.icon:hover{color:gray;transition:linear 0.1s;}
 .icon{background:none;border:none;padding:0;}
-.tableWrap{display:flex;flex-direction:column;align-items:center;}
-#add{margin-top:8px;}
+.tableWrap{display:flex;flex-direction:row;align-items:flex-end;}
+.clientsWrap{display:flex;align-items:center;gap:4px;}
+#add{margin-left:8px;margin-bottom:1px;height:30px;width:30px;}
+#add span{position:relative;left:-6px;font-size:20px;}
 )rawliteral";
 #endif
